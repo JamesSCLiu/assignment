5619f97929eb5dea9cc6984cef25594e12ae1974
James Liu <jameshaha2009@gmail.com>
2020-08-13 20:16:45 +0800

    added guessinggame.sh

---
+++ guessinggame.sh
@@ -0,0 +1,42 @@
+# This program allow user to guess the number of files in the current directory
+# Reponse will be "Too high.", "Too low." or "Congratulation!"
+
+no_file=$(ls | wc -l)
+echo "Guess how many files in the current directory: "
+read input
+
+# Function to validate if the input is an integer
+function validate {
+	re='^[0-9]+$'
+	while ! [[ $input2 =~ $re ]]
+	do
+		echo "Please enter an integer."
+		echo "Guess how many files in the current directory: "
+		read input3
+		let input2=$input3
+	done
+}
+
+# function to try again
+function try_again {
+	echo "Guess how many files in the current directory: "
+	read input2
+	validate
+	let input=$input2
+}
+
+# main while loop
+while [[ $input -ne $no_file ]]
+do
+	if [[ $input -lt $no_file ]]
+	then
+		echo "Too low."
+		try_again
+	elif [[ $input -gt $no_file ]]
+	then
+		echo "Too high."
+		try_again
+	fi
+done
+
+echo "Congratulaion!"
